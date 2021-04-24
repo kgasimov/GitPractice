37a1cd92bed0ac1401d5f346e89380b61d7a63ad
Kamran Gasimov <kgasimov@gmail.com>
2021-04-24 16:51:42 -0500

    adding more work

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 
         System.out.println("Hello world");
         System.out.println("Adding Github Now");
+        System.out.println("Third line, go to git hub");
     }
 }
